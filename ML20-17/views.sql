16cfedd6be46ac27497ab25aa05a632ee0d11a61
ocolladoep <omar.collado@ep.net.pe>
2025-04-30 18:59:21 -0500

    ML20-17: update view

--- ML20-17/views.sql
+++ ML20-17/views.sql
@@ -1,30 +1,37 @@
-CREATE ALGORITHM=UNDEFINED 
-DEFINER=`rootDev`@`%` 
+CREATE ALGORITHM = UNDEFINED 
+DEFINER = `rootDev`@`%` 
 SQL SECURITY DEFINER 
-VIEW `v_notification_webhook_dispersion` AS 
+VIEW `v_notification_webhook_dispersion` AS
+
 WITH `T1` AS (
     SELECT 
         `nm`.`payout_order_id` AS `payout_order_id`,
         `nm`.`status` AS `status`,
         `nm`.`notification_response` AS `notification_response`,
-        `nm`.`created_at` AS `created_at` 
+        `nm`.`created_at` AS `created_at`
     FROM 
-        (`notification_monnet_webhook` `nm` 
+        `notification_monnet_webhook` `nm`
         LEFT JOIN (
-            `payout_order_responses` `prq` 
-            LEFT JOIN `payout_order_requests` `pr` ON (`pr`.`id` = `prq`.`payout_request_id`)
-        ) ON (`nm`.`payout_order_id` = `pr`.`order_id`))
-) 
+            `payout_order_responses` `prq`
+            LEFT JOIN `payout_order_requests` `pr`
+                ON `pr`.`id` = `prq`.`payout_request_id`
+        )
+        ON `nm`.`payout_order_id` = `pr`.`order_id`
+)
+
 SELECT 
+    `dfd`.`id` AS `dispersion_file_detail_id`,
     `prq`.`id` AS `id_payout_order_response`,
-    `pr`.`document_number` AS `document_number`,
-    `pr`.`name` AS `name`,
-    `pr`.`last_name` AS `last_name`,
+    `dfd`.`document_number` AS `document_number`,
+    `dfd`.`name` AS `name`,
+    `dfd`.`lastname` AS `last_name`,
     `pr`.`account_number_cci` AS `account_number_cci`,
     `bd`.`id` AS `beneficiary_document_id`,
     `bd`.`type` AS `beneficiary_document_type`,
-    `pr`.`amount` AS `amount`,
+    `dfd`.`amount` AS `amount`,
     `pr`.`currency` AS `currency`,
+    `dfd`.`is_uploaded` AS `uploaded`,
+    `dfd`.`upload_error` AS `upload_error`,
     `prq`.`error_code` AS `error_code`,
     `prq`.`error_message` AS `error_message`,
     `pr`.`created_at` AS `request_created_at`,
@@ -36,10 +43,20 @@ SELECT
     `webhook`.`status` AS `final_status`,
     JSON_EXTRACT(`webhook`.`notification_response`, '$.errors[0].message') AS `detail_error`,
     `prq`.`id_row_file` AS `id_row_file`,
-    `prq`.`id_dispersion_file_history` AS `id_dispersion_file_history` 
+    `prq`.`id_dispersion_file_history` AS `id_dispersion_file_history`,
+    `dfd`.`channel_type` AS `channel_type`,
+    `dfd`.`description` AS `description`
 FROM 
-    ((((`payout_order_responses` `prq` 
-    LEFT JOIN `payout_order_requests` `pr` ON (`pr`.`id` = `prq`.`payout_request_id`)) 
-    LEFT JOIN `beneficiary_document` `bd` ON (`pr`.`document_type` = `bd`.`parameter`)) 
-    LEFT JOIN `beneficiary_bank` `bb` ON (`pr`.`bank_code` = `bb`.`code`)) 
-    LEFT JOIN `T1` `webhook` ON (`webhook`.`payout_order_id` = `pr`.`order_id`);
+    `dispersion_file_detail` `dfd`
+    JOIN `dispersion_file_history` `dfh`
+        ON `dfd`.`id_dispersion_file_history` = `dfh`.`id`
+    LEFT JOIN `payout_order_requests` `pr`
+        ON `pr`.`order_id` = `dfd`.`id_row_file`
+    LEFT JOIN `payout_order_responses` `prq`
+        ON `pr`.`id` = `prq`.`payout_request_id`
+    LEFT JOIN `beneficiary_document` `bd`
+        ON `dfd`.`document_type` = `bd`.`type`
+    LEFT JOIN `beneficiary_bank` `bb`
+        ON `pr`.`bank_code` = `bb`.`code`
+    LEFT JOIN `T1` `webhook`
+        ON `webhook`.`payout_order_id` = `pr`.`order_id`;
